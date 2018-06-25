dcc7a19125ed370ef3b30c3119b75aabc4c7e3d6
Christy Mar Orfano <32087107+orfanochristy@users.noreply.github.com>
2018-06-25 09:35:04 +0800

    Update multi_inherit.py

--- multi_inherit.py
+++ multi_inherit.py
@@ -1,31 +1,31 @@
  #multiple inheritance
  class Subject:
-	def __init__(self, subName):
-		print (subName, "also handles different classes ")
+	def __init__(self, subj_name):
+		print (subj_name, "also handles different classes ")
 
 		
  class Math(Subject):
-	def __init__(self, mTeacher):
-		print (mTeacher, "is my Math Teacher")
-		super().__init__(mTeacher)
+	def __init__(self, math_teacher):
+		print (math_teacher, "is my Math Teacher")
+		super().__init__(math_teacher)
 
 	
 class Science(Subject):
-	def __init__(self, sTeacher):
-		print (sTeacher, "is my Science Teacher")
-		super().__init__(sTeacher)
+	def __init__(self, science_teacher):
+		print (science_teacher, "is my Science Teacher")
+		super().__init__(science_teacher)
 
 		
 class English(Subject):
-	def __init__(self, eTeacher):
-		print(eTeacher, "is my English Teacher")
-		super().__init__(eTeacher)
+	def __init__(self, english_teacher):
+		print(english_teacher, "is my English Teacher")
+		super().__init__(english_teacher)
 
 		
 class Art(Subject):
-	def __init__(self, aTeacher):
-		print(aTeacher, "is my Art Teacher")
-		super().__init__(aTeacher)
+	def __init__(self, art_teacher):
+		print(art_teacher, "is my Art Teacher")
+		super().__init__(art_teacher)
 
  
 class Brown(Science, Art):
